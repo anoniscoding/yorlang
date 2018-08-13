5543e7e115bbaf1c4fd0916f97c314e5d3bb3a7b
karounwi.anuoluwapo <karounwianuoluwapo@gmail.com>
2018-08-13 18:56:40 +0100

    modified inputstream test, removed redundant code variable

--- tests/inputstream.test.js
+++ tests/inputstream.test.js
@@ -3,18 +3,19 @@ const constants = require("../constants.js");
 
 describe("InputStream Tests", () => {
     let inputStream;
-    const code = `tí a = 3;`;
 
     beforeEach(() => {
-        inputStream = new InputStream(code);
+        inputStream = new InputStream();
     });
 
     test("Peek - It should peek at the next character without discarding it from the stream", () => {
+        inputStream.code = "tí";
         expect(inputStream.peek()).toBe("t");
         expect(inputStream.next()).toBe("t");
     });
 
     test("Next - It should read in the next character and discard it from the stream", () => {
+        inputStream.code = "tí";
         expect(inputStream.next()).toBe("t");
         expect(inputStream.peek()).toBe("í");
     });
@@ -22,20 +23,23 @@ describe("InputStream Tests", () => {
     test("ThrowError - It should throw an error message while specifying the location of the error accurately", () => {
         inputStream.code = "\n";
         inputStream.next(); //read in the new line character
+        
+        const errorMsg = "Testing error msg";
 
         expect(() => {
-            inputStream.throwError("Testing error msg");
-        }).toThrow(`There's an error at line 2 near column 0.\nTesting error msg`);
+            inputStream.throwError(errorMsg);
+        }).toThrow(`There's an error at line 2 near column 0.\n${errorMsg}`);
     });
 
     test("isNotEndOfFile - It should confirm that the inputstream has not read in the last char in the file", () => {
+        inputStream.code = "tí";
         expect(inputStream.isNotEndOfFile()).toBe(true);
-    })
+    });
 
     test("isEndOfFile - It should confirm that the inputstream has read in the last char in the file", () => {
         inputStream.code = "";
 
         expect(inputStream.isEndOfFile()).toBe(true);
-    })
+    });
 
 });
