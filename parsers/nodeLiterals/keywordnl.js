855da6ae972951898ebb73211b241db7bf34fb29
karounwi.anuoluwapo <karounwianuoluwapo@gmail.com>
2018-08-19 17:01:44 +0100

    Fixed bug. keywordnl was still referencing parseleaf which has been replaced with leafNl

--- parsers/nodeLiterals/keywordnl.js
+++ parsers/nodeLiterals/keywordnl.js
@@ -1,4 +1,5 @@
 const constants = require("../../constants.js");
+const leafNl = require("./leafnl.js");
 const BaseNode = require("../basenode.js");
 
 class KeywordNl extends BaseNode {
@@ -6,7 +7,7 @@ class KeywordNl extends BaseNode {
     getNode() {
         //if keyword is boolean
         if ([constants.KW.OOTO, constants.KW.IRO].indexOf(this.lexer.peek().value) >= 0) {
-            return this.parseLeaf();
+            return leafNl.getNode.call(this);
         }
             
         this.lexer.throwError(`Expecting yorlang boolean(iró|òótó) but found ${token.value}`);
