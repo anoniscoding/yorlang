36284d350642b7b1020c44dba495fb0db61b5da1
karounwi.anuoluwapo <karounwianuoluwapo@gmail.com>
2018-09-20 22:18:52 +0100

    registered iRemainder interpreter

--- interpreters/interpreters.js
+++ interpreters/interpreters.js
@@ -3,9 +3,13 @@ const iPlus = require("./iplus.js");
 const iMinus = require("./iminus.js");
 const iDivide = require("./idivide.js");
 const iMultiply = require("./imultiply.js");
+const iRemainder = require("./iremainder.js");
 
 const interpreters = {};
 interpreters[Symbol.for(constants.SYM.PLUS)] = iPlus;
 interpreters[Symbol.for(constants.SYM.MINUS)] = iMinus;
 interpreters[Symbol.for(constants.SYM.DIVIDE)] = iDivide;
 interpreters[Symbol.for(constants.SYM.MULTIPLY)] = iMultiply;
+interpreters[Symbol.for(constants.SYM.REMAINDER)] = iRemainder;
+
+module.exports = interpreters;
