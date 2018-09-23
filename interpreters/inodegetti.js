ecb7414aaec390d84ab45466a00465c1dc1cd479
karounwi.anuoluwapo <karounwianuoluwapo@gmail.com>
2018-09-23 18:16:13 +0100

    trivial change- added braces

--- interpreters/inodegetti.js
+++ interpreters/inodegetti.js
@@ -4,8 +4,9 @@ class INodeGetTi extends IBase {
 
     interpreteNode(node) {
         for (let index = this.scopeStack.length - 1; index >= 0; index--) {
-            if (this.environment().getTi(this.scopeStack[index], node.name) != undefined)
+            if (this.environment().getTi(this.scopeStack[index], node.name) != undefined) {
                 return this.environment().getTi(this.scopeStack[index], node.name);
+            }
         }    
 
         throw new Error(`Variable ${node.name} is not defined`);
