d10ff60cac3ca49c19e2e97f18b6a4eac8620081
karounwi.anuoluwapo <karounwianuoluwapo@gmail.com>
2018-10-19 12:26:01 +0100

    Refactor - cleaned up inodearray, removed redundant switch case, added interpreter for arraynodeelem

--- interpreters/inodearray.js
+++ interpreters/inodearray.js
@@ -4,20 +4,10 @@ const iNodeGetTi = require("./inodegetti.js");
 class INodeArray extends IBase {
 
     interpreteNode(arrayNode) {
-        const arr = []; const iNodeArray = new INodeArray();
+        const arr = [];
 
         arrayNode.body.forEach(arrayItemNode => {
-            switch (arrayItemNode.operation) {
-                case constants.GET_TI :
-                    arr.push(iNodeGetTi.interpreteNode.call(this, arrayItemNode)); break;
-                case constants.ARRAY : //for multidimensional arrays
-                    arr.push(iNodeArray.interpreteNode.call(this,arrayItemNode)); break;
-                case constants.ARRAY_ELEM :
-                    const arrayLiteral = iNodeGetTi.interpreteNode.call(this, arrayItemNode);
-                    arr.push(arrayLiteral[arrayItemNode.index]); break;
-                default : //array item is a string/num/float
-                    arr.push(arrayItemNode.value);
-            }
+            arr.push(this.evaluateNode(arrayItemNode));
         });
 
         return arr;
---
+++ interpreters/inodearrayelem.js
@@ -0,0 +1,14 @@
+const IBase = require("./ibase.js");
+const contansts = require("../constants.js");
+const iNodeGetTi = require("./inodegetti.js");
+
+class INodeArrayElement extends IBase {
+
+    interpreteNode(node) {
+        const tiNode = {name: node.name, operation: contansts.GET_TI}
+        const arrayLiteral = this.evaluateNode(tiNode);
+        return arrayLiteral[node.index];
+    }
+}
+
+module.exports = new INodeArrayElement();
