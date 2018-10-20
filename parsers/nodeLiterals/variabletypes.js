5d761e4f62a5c06344159dd376470c18544b7af4
karounwi.anuoluwapo <karounwianuoluwapo@gmail.com>
2018-10-20 22:18:09 +0100

    refactor - made variable type registration point conform to the code standard

--- parsers/nodeLiterals/variabletypes.js
+++ parsers/nodeLiterals/variabletypes.js
@@ -1,9 +1,7 @@
 const constants = require("../../constants.js");
-const arrayNl = require("./arraynl.js");
-const callIseNl = require("./callIseNl.js");
 
 const variableTypes = {};
-variableTypes[constants.SYM.L_BRACKET] = callIseNl;  //when current variable is a function call
-variableTypes[constants.SYM.L_SQ_BRACKET] = arrayNl; //when current variable is an array element
+variableTypes[constants.SYM.L_BRACKET] = require("./callIseNl.js");  //when current variable is a function call
+variableTypes[constants.SYM.L_SQ_BRACKET] = require("./arraynl.js"); //when current variable is an array element
 
 module.exports = variableTypes;
