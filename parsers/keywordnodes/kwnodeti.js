3efbf4b9dd7e64adfd8be9708f96a5192fdd1fd1
karounwi.anuoluwapo <karounwianuoluwapo@gmail.com>
2018-10-22 17:59:57 +0100

    Refactor- removed duplicated code in kwnodeti, delegated task to variablenl

--- parsers/keywordnodes/kwnodeti.js
+++ parsers/keywordnodes/kwnodeti.js
@@ -1,35 +1,21 @@
 const constants = require("../../constants.js");
 const BaseNode = require("../basenode.js");
-const variableNlTypes = require("../nodeLiterals/variablenltypes.js");
+const variableNl = require("../nodeLiterals/variablenl.js");
 
 class KwNodeTi extends BaseNode {
 
     getNode() {
         this.skipKeyword(constants.KW.TI);
-               
-        const varName = this.parseVarname();
 
         const node =  {};
         node.operation = constants.SYM.ASSIGN;
-        node.left = KwNodeTi.getLeftNode(this, varName) || varName;
+        const varNode = variableNl.getNode.call(this);
+        node.left = (varNode.operation === constants.GET_TI) ? varNode.name : varNode;
         this.skipOperator(constants.SYM.ASSIGN);
         node.right  = this.parseExpression();
         this.skipPunctuation(constants.SYM.STATEMENT_TERMINATOR);
 
         return node;
-    }
-
-    static getLeftNode(context, varName) {
-        const nextTokenValue = context.lexer.peek().value;
-
-        //if current variable initialization is not a function call
-        if (nextTokenValue != constants.SYM.L_BRACKET) {
-            if (variableNlTypes[nextTokenValue] != undefined) { //current variable could be an array element or object property etc
-                const variableNlType = variableNlTypes[nextTokenValue];
-                if (variableNlType instanceof BaseNode) return variableNlType.getNode.call(context, {value: varName});
-                else throw new Error(`Dependency ${variableNlType} must be of type BaseNode`);
-            }
-        }
     }
 }
 
