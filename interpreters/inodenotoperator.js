082607fef27114377e963c3b1aeb0df4f7a2a0c4
karounwi.anuoluwapo <karounwianuoluwapo@gmail.com>
2018-11-02 21:54:53 +0100

    Refactor- made changes to not opertor logic

--- interpreters/inodenotoperator.js
+++ interpreters/inodenotoperator.js
@@ -4,9 +4,7 @@ const constants = require("../constants.js");
 class INodeNotOperator extends IBase {
 
     interpreteNode(node) {
-        const bodyValue = this.evaluateNode(node.body);
-        if (bodyValue === constants.KW.OOTO) return constants.KW.IRO;
-        if (bodyValue === constants.KW.IRO) return constants.KW.OOTO;
+        if (this.evaluateNode(node.body) === constants.KW.IRO) return constants.KW.OOTO;
         
         //There's no need for another case because bodyValue will always have a value
         return constants.KW.IRO;
