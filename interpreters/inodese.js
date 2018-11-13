f2078759668e8b8171248061b0fb4d22fa671de6
karounwi.anuoluwapo <karounwianuoluwapo@gmail.com>
2018-11-13 01:14:54 +0100

    Refactoring- renamed some variables

--- interpreters/inodese.js
+++ interpreters/inodese.js
@@ -5,20 +5,18 @@ class INodeSe extends IBase {
 
     interpreteNode(node) {
         if (this.evaluateNode(node.condition) === constants.KW.OOTO) {
-            return INodeSe.runSeBody(this, node.then);
-        } else {
-            if (node.else != undefined) {
-                return INodeSe.runSeBody(this, node.else);
-            }
+            return INodeSe.runBody(this, node.then);
+        } else if (node.else != undefined) {
+            return INodeSe.runBody(this, node.else);
         }
     }
 
-    static runSeBody(context, seBody) {
+    static runBody(context, body) {
         //cater for 'tabi se' block
-        if (!(seBody instanceof Array)) context.evaluateNode(seBody);
+        if (!(body instanceof Array)) context.evaluateNode(body);
 
-        for (let i = 0; i < seBody.length; i++) {
-            const returnedValue = context.evaluateNode(seBody[i]);
+        for (let i = 0; i < body.length; i++) {
+            const returnedValue = context.evaluateNode(body[i]);
 
             if (returnedValue === constants.KW.KURO) return constants.KW.KURO;
             else if (returnedValue != undefined) return returnedValue; //it's an ise pada value
