3346f65167e7ce89f1cda9182d3d1812976b43c1
karounwi.anuoluwapo <karounwianuoluwapo@gmail.com>
2018-11-18 09:56:03 +0100

    Refactoring - Reoredered the logic of KwNodeIse for readability

--- parsers/keywordnodes/kwnodeise.js
+++ parsers/keywordnodes/kwnodeise.js
@@ -4,20 +4,21 @@ const BaseNode = require("../basenode.js");
 class KwNodeIse extends BaseNode {
 
     getNode() {
-        if (!KwNodeIse.isExpectedIseDeclaration(this))
-            this.lexer.throwError("Cannot create a yorlang function within a non function block");
-        
-        this.skipKeyword(constants.KW.ISE);
+        if (KwNodeIse.isExpectedIseDeclaration(this)) {
+            this.skipKeyword(constants.KW.ISE);
 
-        return {
-            operation: constants.KW.ISE,
-            name: this.parseVarname(),
-            paramTokens: this.parseDelimited( 
-                constants.SYM.L_BRACKET , constants.SYM.R_BRACKET, constants.SYM.COMMA,
-                this.getTokenThatSatisfiesPredicate.bind(this), (token) => token.type === constants.VARIABLE
-            ),
-            body: this.parseBlock(constants.KW.ISE),
-        };    
+            return {
+                operation: constants.KW.ISE,
+                name: this.parseVarname(),
+                paramTokens: this.parseDelimited( 
+                    constants.SYM.L_BRACKET , constants.SYM.R_BRACKET, constants.SYM.COMMA,
+                    this.getTokenThatSatisfiesPredicate.bind(this), (token) => token.type === constants.VARIABLE
+                ),
+                body: this.parseBlock(constants.KW.ISE),
+            };
+        }
+        
+        this.lexer.throwError("Cannot create a yorlang function within a non function block");   
     }
 
     static isExpectedIseDeclaration(context) {
