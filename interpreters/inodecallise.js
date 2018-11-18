381d0bc20838ed8dac3f88fb542de952601b1138
karounwi.anuoluwapo <karounwianuoluwapo@gmail.com>
2018-11-18 09:33:17 +0100

    Refactoring - renamed params to paramNodeList in Inodecallise - getIseHelperParams()

--- interpreters/inodecallise.js
+++ interpreters/inodecallise.js
@@ -25,15 +25,15 @@ class INodeCallIse extends IBase {
                 return context.environment().getIse(context.scopeStack()[index], iseName);
             }
         }
-        return null
+        return null;
     }
 
-    static getIseHelperParams(context, params) {
-        const _params = [];
-        params.forEach(param => {
-            _params.push(context.evaluateNode(param))
+    static getIseHelperParams(context, paramNodeList) {
+        const params = [];
+        paramNodeList.forEach(paramNode => {
+            params.push(context.evaluateNode(paramNode));
         });
-        return _params;
+        return params;
     }
 
     static setIseNodeParam(context, iseNodeParamTokens, iseNodeParamValues) {
