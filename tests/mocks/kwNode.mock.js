13c0b2650f2adb169217d1856d3b5899a5367943
Comical Derskeal <27731088+derskeal@users.noreply.github.com>
2018-12-07 00:32:35 +0100

    Added root path joining | Rename to camelCasing

--- tests/mocks/kwNode.mock.js
+++ tests/mocks/kwNode.mock.js
@@ -1,4 +1,6 @@
-const BaseNode = require("../../parsers/basenode.js");
+const path = require('path');
+
+const BaseNode = require(path.join(rootDir, "parsers/baseNode.js"));
 
 //a mock class that does not override getNode
 class KwNodeMock extends BaseNode {  }
