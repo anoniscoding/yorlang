ddfeee1b79ac80474c17009ed72605269bb3b1f5
ddamox <damilolaspencer1001@gmail.com>
2018-12-12 12:33:15 +0100

    variable name changes to video accordion

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,14 +1,11 @@
-let acc = document.getElementsByClassName("video_accordion");
-var i;
+const videoAccordions = document.getElementsByClassName("video_accordion");
+let i;
 
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function () {
+for (i = 0; i < videoAccordions.length; i++) {
+    videoAccordions[i].addEventListener("click", function () {
         this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
-        }
+        const panel = this.nextElementSibling;
+        panel.style.display = (panel.style.display === "block") ? "none" : "block";
+        
     });
 }
