f28745beec5f0cf4325e890ae8239811fd366913
karounwi.anuoluwapo <karounwianuoluwapo@gmail.com>
2018-12-14 15:15:25 +0100

    Refactoring - cleaned up helper_ise_adapter

--- src/interpreters/helpers/helper_ise_adapter.js
+++ src/interpreters/helpers/helper_ise_adapter.js
@@ -1,10 +1,9 @@
 function getFormattedInput (input) {
-    if (Array.isArray(input)) return input;
-
     switch (typeof input) {
     case "string":
     case "number": return input;
     case "boolean": return (input) ? "ooto" : "iro";
+    case "object": if (Array.isArray(input)) return input;
     }
 
     throw new Error("Yorlang system error: invalid result returned from helper function");
