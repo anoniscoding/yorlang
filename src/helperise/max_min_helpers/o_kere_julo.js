aef75c9e306ab1e76866deeb00a93b36fb7522ca
Harjacober <harjacober@gmail.com>
2018-12-19 11:45:57 +0100

    created max and min helper functions

--- src/helperise/max_min_helpers/o_kere_julo.js
+++ src/helperise/max_min_helpers/o_kere_julo.js
@@ -1,23 +1,22 @@
 /**
  * Get the minimum oflist of numbers
  * input can either be an array or a number of arguments seperated by a ','
- * 
  * note that if the first argument is an array, all other arguments are neglected
  * @returns {[number]} minimumNumber
  */
-function o_kere_julo(){
-    var obj = arguments[0]
-    var array = []
-    if(isArray(obj)){
-        array = obj
-    }else{
-        for (var i=0; i < arguments.length; i++) {
-            array.push(arguments[i])
+function oKereJulo () {
+    var obj = arguments[0];
+    var array = [];
+    if (isArray(obj)) {
+        array = obj;
+    } else {
+        for ( var i=0; i < arguments.length; i++ ) {
+            array.push(arguments[i]);
         }
     }
     return Math.min(...array);
 }
-function isArray(obj){
+function isArray (obj) {
     return !!obj && obj.constructor === Array;
 }
 
--- src/helperise/max_min_helpers/o_tobi_julo.js
+++ src/helperise/max_min_helpers/o_tobi_julo.js
@@ -1,24 +1,23 @@
 /**
  * Get the maximum of list of numbers,
  * input can either be an array or a number of arguments seperated by a ','
- * 
  * note that if the first argument is an array, all other arguments are neglected
  * @returns {[number]} maximumNumber
  */
-function o_tobi_julo(){
-    var obj = arguments[0]
-    var array = []
-    if(isArray(obj)){
-        array = obj
-    }else{
-        for (var i=0; i < arguments.length; i++) {
-            array.push(arguments[i])
+function oTobiJulo () {
+    var obj = arguments[0];
+    var array = [];
+    if (isArray(obj)) {
+        array = obj;
+    } else {
+        for ( var i=0; i < arguments.length; i++ ) {
+            array.push(arguments[i]);
 
         }
     }
     return Math.max(...array);
 }
-function isArray(obj){
+function isArray (obj) {
     return !!obj && obj.constructor === Array;
 }
 
