65a17c813d0df16847bf2bc4942539d385cacdc4
Ikechi Michael <mykehell123@gmail.com>
2018-12-28 09:39:05 +0100

    doc: add comments to explain 0-based JS vs vs 1-based yorlang month values

--- src/helperise/datetime_helpers/aago.js
+++ src/helperise/datetime_helpers/aago.js
@@ -7,11 +7,11 @@ function aago (args) {
     if (Array.isArray(args)) {
         // make sure an array can be passed in as the first param
         if (Array.isArray(args[0])) args = args[0];
-        if (args[1]) --args[1]; // make sure month is 1-based rather than 0-based
+        if (args[1]) --args[1]; // convert month value to 0-based index for JavaScript
         const date = new Date(...args);
         return [
             date.getFullYear(),
-            date.getMonth() + 1,
+            date.getMonth() + 1, // increment 0-based index for use by Yorlang
             date.getDate(),
             date.getHours(),
             date.getMinutes(),
